f52f3da115f390059b7755a2521f1d00e57ffce1
wuttinanhi <wuttinanhi@gmail.com>
2021-02-26 14:34:34 +0700

    add history feature

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
+import axios from "axios";
 
 async function getMembers(pageName: string): Promise<number> {
   const url = `https://en-gb.facebook.com/groups/${pageName}/about`;
@@ -32,6 +33,28 @@ async function writeFile(data: Record<string, any>) {
   });
 }
 
+async function writeHistoryFile(data: Record<string, any>) {
+  return new Promise((resolve) => {
+    fs.writeFile(
+      "./public/history.json",
+      JSON.stringify(data, null, 2),
+      "utf8",
+      resolve
+    );
+  });
+}
+
+async function fetchPreviousHistory(): Promise<Record<string, any>[]>{
+  try {
+    const response = await axios.get("https://9arm-vs-kob-api.vercel.app/history.json");
+    if(response.data === null || response.data.history === undefined ) return [];
+    return response.data.history;
+  } catch (error) {
+    return [];
+  }
+}
+
+
 const browser = await puppeteer.launch();
 
 const group9arm = await getMembers("9arm.community");
@@ -44,3 +67,15 @@ const data = { group9arm, groupKob, diff, updatedAt: new Date().toISOString() };
 console.log(data);
 
 await writeFile(data);
+
+const history = await fetchPreviousHistory();
+const lastDataFromHistory  = history[history.length - 1]; 
+
+if( lastDataFromHistory && data.diff === lastDataFromHistory.diff){
+  console.log("Same data no update.");
+}else{
+  console.log("Updating history.");
+  history.push(data);
+  const slicedHistory = history.slice(-5);
+  await writeHistoryFile(slicedHistory);
+}
