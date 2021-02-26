fb95d8d1584a18a11a0c5ae3ea48d7bf421f3ef5
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2021-02-26 21:55:57 +0700

    History feature 📜 (new) (#3)

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,29 @@ async function writeFile(data: Record<string, any>) {
   });
 }
 
+async function writeHistoryFile(data: Record<string, any>) {
+  return new Promise((resolve) => {
+    fs.writeFile(
+      "./public/history.json",
+      JSON.stringify(data, null, 2),
+      "utf8",
+      resolve
+    );
+  });
+}
+
+async function fetchPreviousHistory(): Promise<Record<string, any>[]>{
+    return new Promise((resolve) => {
+      fs.readFile("./public/history.json", 'utf8', (err, data) => {
+        if (err) return resolve([]);
+        const json = JSON.parse(data);
+        if (json['history'] === undefined) return resolve([]);
+        return resolve(json['history']);
+      })
+    });
+}
+
+
 const browser = await puppeteer.launch();
 
 const group9arm = await getMembers("9arm.community");
@@ -44,3 +67,16 @@ const data = { group9arm, groupKob, diff, updatedAt: new Date().toISOString() };
 console.log(data);
 
 await writeFile(data);
+
+const historyLimit = 100;
+const history = await fetchPreviousHistory();
+const lastDataFromHistory  = history[history.length - 1]; 
+
+if( lastDataFromHistory && data.diff === lastDataFromHistory.diff){
+  console.log("Same data no update.");
+}else{
+  console.log("Updating history.");
+  history.push(data);
+  const slicedHistory = history.slice(-historyLimit);
+  await writeHistoryFile({ history: slicedHistory });
+}
